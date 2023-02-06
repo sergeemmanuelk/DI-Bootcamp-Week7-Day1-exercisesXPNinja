484673a079581df6944a024288148fc17127a7be
sergeemmanuelk <serge_em@outlook.fr>
2023-02-06 18:42:26 +0000

    Initial commit

---
+++ exercise1/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Dog");
+        Dog d = new Dog();
+        d.bark();
+        d.eat();
+
+        System.out.println("*************");
+
+        System.out.println("Cat");
+        Cat c = new Cat();
+        c.meow();
+        c.eat();
+    }
+}
